56d124937f7e889b18f6970270d238864311d933
Sam Choy <samchoy88@hotmail.com>
2014-12-05 03:19:29 +0800

    BlinkyTile TestRig for production
    - Bugs fixed
    - automatic program address (1-14) then verify address and test color
    - added erase command
    - added test color command

--- tile_testjig/TestRig/TestRig.ino
+++ tile_testjig/TestRig/TestRig.ino
@@ -2,12 +2,29 @@
 #define COMMAND_PROGRAM_ADDRESSES     'p'    // Run the program address routine
 #define COMMAND_OUTPUT_COLOR          's'    // Set all outputs to a specific color (0=red, 1=green, 2=blue)
 #define COMMAND_OUTPUT_IDENTIFY       'i'    // Identify one output 
+#define COMMAND_ERASE_ADDRESSES       'e'    // Erase address routine
+#define COMMAND_TEST_COLOR            't'    // Test color routine 
 
 #define SWITCH_PIN    9
 
 
-#define MAX_OUTPUTS  50     // Number of address outputs available
+#define MAX_OUTPUTS  14     // Number of address outputs available
 #define OUTPUT_COUNT 14     // Number of address outputs enabled
+
+
+#define BIT_LENGTH                 (4)
+#define BREAK_LENGTH              (95)    //(88)
+#define MAB_LENGTH                (12)    //(8)
+
+#define PREFIX_BREAK_TIME        (BREAK_LENGTH)
+#define PREFIX_MAB_TIME          (PREFIX_BREAK_TIME + MAB_LENGTH)
+
+#define FRAME_START_BIT_TIME     (BIT_LENGTH)
+#define FRAME_STOP_BITS_TIME     (FRAME_START_BIT_TIME + 8*BIT_LENGTH + 2*BIT_LENGTH)
+
+
+#define GROUPS        2    // These should equal the MAX_OUTPUTS
+#define GROUP_SIZE    7
 
 //// Programmer pins
 //const int csPin      = 10;
@@ -25,12 +42,15 @@ const int passLedPin = 23;
 static volatile uint8_t *dmxPort;
 static uint8_t dmxBit = 0;
 
+#define COMMAND_LENGTH  3
+char inputBuffer[COMMAND_LENGTH];
+int bufferPosition = 0;
 
 #define BYTES_PER_PIXEL    3
 uint8_t dataArray[1 + MAX_OUTPUTS*BYTES_PER_PIXEL];    // Storage for DMX output (0 is the start frame)
 
 // Address pin connections
-const int addressPins[OUTPUT_COUNT] = {
+const int addressPins[MAX_OUTPUTS] = {
   21,      // 1
   20,      // 2
   19,      // 3
@@ -44,11 +64,11 @@ const int addressPins[OUTPUT_COUNT] = {
   6,       // 11
   5,       // 12
   4,       // 13
-  3,       // 14
+  3       // 14
 };
 
 // Output addresses to program
-const int addresses[OUTPUT_COUNT] = {
+const int addresses[MAX_OUTPUTS] = {
   1,
   2,
   3,
@@ -62,14 +82,14 @@ const int addresses[OUTPUT_COUNT] = {
   11,
   12,
   13,
-  14,
+  14
 };
 
 // Data table of bytes we need to send to program the addresses
 // Note that this table needs to be initialized by a call to 
 // makeAddressTable() before using.
-#define PROGRAM_ADDRESS_FRAMES 1+3
-int addressProgrammingData[OUTPUT_COUNT][PROGRAM_ADDRESS_FRAMES];
+#define PROGRAM_ADDRESS_FRAMES 4
+int addressProgrammingData[MAX_OUTPUTS][PROGRAM_ADDRESS_FRAMES];
 
 
 void writePixel(int pixel, int r, int g, int b) {
@@ -89,7 +109,7 @@ void setAddresses(int level, int offset, int count) {
 
 
 void setAddresses(int level) {
-  for(int address = 0; address < MAX_OUTPUTS; address++) {
+  for(int address = 0; address < OUTPUT_COUNT; address++) {
     digitalWriteFast(addressPins[address], level);
   }
 }
@@ -105,34 +125,37 @@ uint8_t flipEndianness(uint8_t input) {
   return output;
 }
 
+//=============================================================================================================
+//    make Address Table
+//=============================================================================================================
 
 // Make a table of the values we have to send to program the addressess
 void makeAddressTable() {
-  for(int address = 0; address < OUTPUT_COUNT; address++) {
+  for(int address = 0; address < MAX_OUTPUTS; address++) {
     // WS2822S (from datasheet)
     int channel = (addresses[address]-1)*3+1;
-    addressProgrammingData[address][1] = 0;
+    addressProgrammingData[address][0] = 0;
     addressProgrammingData[address][1] = flipEndianness(channel%256);
     addressProgrammingData[address][2] = flipEndianness(240 - (channel/256)*15);
     addressProgrammingData[address][3] = flipEndianness(0xD2);
   }
 }
 
+//=============================================================================================================
+//    reset Power
+//=============================================================================================================
 
-#define BIT_LENGTH                 (4)
-#define BREAK_LENGTH              (95)    //(88)
-#define MAB_LENGTH                (12)    //(8)
+void resetPower(void)
+{
+    digitalWrite(powerPin, HIGH);
+    delay(250);
+    digitalWrite(powerPin, LOW);
+    delay(100);
+}
 
-#define PREFIX_BREAK_TIME        (BREAK_LENGTH)
-#define PREFIX_MAB_TIME          (PREFIX_BREAK_TIME + MAB_LENGTH)
-
-#define FRAME_START_BIT_TIME     (BIT_LENGTH)
-#define FRAME_STOP_BITS_TIME     (FRAME_START_BIT_TIME + 8*BIT_LENGTH + 2*BIT_LENGTH)
-
-
-#define GROUPS        2    // These multiplied together should equal the OUTPUT_COUNTS
-#define GROUP_SIZE    8
-
+//=============================================================================================================
+//    dmxSendByte
+//=============================================================================================================
 
 void dmxSendByte(uint8_t value)
 {
@@ -158,21 +181,110 @@ void dmxSendByte(uint8_t value)
 }
 
 
-
-// Send programming data
-void programAddresses() {
+//=============================================================================================================
+//    erase Addresses
+//=============================================================================================================
+void eraseAddresses(){
+digitalWrite(failLedPin, HIGH);
+  digitalWrite(passLedPin, LOW);
+  Serial.println("Start Program addresses");
+  resetPower();
   
   // Pull data pin low
   digitalWriteFast(dataPin, LOW);
   
+  
   // Set address pins to outputs
-  for(int i = 0; i < OUTPUT_COUNT; i++) {
+  for(int i = 0; i < MAX_OUTPUTS; i++) {
     pinMode(addressPins[i], OUTPUT);
-    digitalWriteFast(addressPins[i], LOW);
+    digitalWrite(addressPins[i], HIGH);
+  }
+    
+  for(int group = 0; group < GROUPS; group++) {
+    
+    delay(50);
+    Serial.print("Group = ");
+    Serial.println(group, DEC);
+    setAddresses(LOW, group*GROUP_SIZE, GROUP_SIZE);                 // BREAK
+    delay(500);
+  
+    noInterrupts();
+    elapsedMicros startTime;            // Microsecond timing starts here
+    
+    setAddresses(HIGH, group*GROUP_SIZE, GROUP_SIZE);                 // MAB
+    while(startTime < MAB_LENGTH) {};
+    
+    // Program the address data in parallel
+    for(int frame = 0; frame < PROGRAM_ADDRESS_FRAMES; frame++) {
+      elapsedMicros addressFrameStartTime;
+      
+      setAddresses(LOW, group*GROUP_SIZE, GROUP_SIZE);    // Start bit
+      while(addressFrameStartTime < FRAME_START_BIT_TIME) {};
+    
+      for(int bit = 0; bit < 8; bit++) {  // data bits
+        for(int address = 0; address < GROUP_SIZE; address++) {
+          int groupAddress = address + group*GROUP_SIZE;
+          
+          digitalWriteFast(addressPins[groupAddress],
+                      (addressProgrammingData[0][frame] >> bit) & 0x01);
+        }
+        while(addressFrameStartTime < (FRAME_START_BIT_TIME+(bit+1)*BIT_LENGTH)) {};
+      }
+      
+      setAddresses(HIGH, group*GROUP_SIZE, GROUP_SIZE);    // Stop bit
+      while(addressFrameStartTime < (FRAME_STOP_BITS_TIME)) {};
+    }
+    
+    setAddresses(LOW, group*GROUP_SIZE, GROUP_SIZE);  // BREAK
+    delay(50);
+    
+    setAddresses(HIGH, group*GROUP_SIZE, GROUP_SIZE);    // MTBP
+    delayMicroseconds(88);
+    
+    interrupts();
   }
   
-  for(int group = 0; group < GROUPS; group++) {
+  // Set the address pins to inputs again
+  for(int i = 0; i < OUTPUT_COUNT; i++) {
+    pinMode(addressPins[i], INPUT);
+  }
   
+  // And set the data pin low
+  digitalWriteFast(dataPin, HIGH);
+  
+  resetPower();
+  Serial.println("Programming END");
+  digitalWrite(failLedPin, LOW);
+  digitalWrite(passLedPin, HIGH);  
+}
+
+//=============================================================================================================
+//    program Addresses
+//=============================================================================================================
+
+// Send programming data
+void programAddresses() { 
+  digitalWrite(failLedPin, HIGH);
+  digitalWrite(passLedPin, LOW);
+  Serial.println("Start Program addresses");
+  resetPower();
+  
+  // Pull data pin low
+  digitalWriteFast(dataPin, LOW);
+  
+  
+  // Set address pins to outputs
+  for(int i = 0; i < MAX_OUTPUTS; i++) {
+    pinMode(addressPins[i], OUTPUT);
+    digitalWrite(addressPins[i], HIGH);
+  }
+    
+  for(int group = 0; group < GROUPS; group++) {
+    
+    delay(50);
+    Serial.print("Group = ");
+    Serial.println(group, DEC);
+    setAddresses(LOW, group*GROUP_SIZE, GROUP_SIZE);                 // BREAK
     delay(500);
     //delayMicroseconds(BREAK_LENGTH);
   
@@ -203,6 +315,8 @@ void programAddresses() {
       while(addressFrameStartTime < (FRAME_STOP_BITS_TIME)) {};
     }
     
+    setAddresses(LOW, group*GROUP_SIZE, GROUP_SIZE);  // BREAK
+    delay(50);
     
     setAddresses(HIGH, group*GROUP_SIZE, GROUP_SIZE);    // MTBP
     delayMicroseconds(88);
@@ -217,13 +331,22 @@ void programAddresses() {
   
   // And set the data pin low
   digitalWriteFast(dataPin, HIGH);
+  
+  resetPower();
+  Serial.println("Programming END");
+  digitalWrite(failLedPin, LOW);
+  digitalWrite(passLedPin, HIGH);
+  
 }
 
 
-
+//=============================================================================================================
+//    Draw
+//=============================================================================================================
 
 // Send a DMX frame with new data
 void draw() {
+  resetPower();
   noInterrupts();
   
   elapsedMicros prefixStartTime;
@@ -236,34 +359,46 @@ void draw() {
   
   // For each address
   for(int frame = 0; frame < 1 + OUTPUT_COUNT*BYTES_PER_PIXEL; frame++) {    
-//    noInterrupts();
-//    
-//    elapsedMicros frameStartTime;
-//    
-//    digitalWriteFast(dataPin, LOW);    // Start bit
-//    while(frameStartTime < FRAME_START_BIT_TIME) {};
-//  
-//    for(int bit = 0; bit < 8; bit++) {  // data bits
-//      digitalWriteFast(dataPin, (dataArray[frame] >> bit) & 0x01);
-//      while(frameStartTime < (FRAME_START_BIT_TIME+(bit+1)*BIT_LENGTH)) {};
-//    }
-//    
-//    digitalWriteFast(dataPin, HIGH);    // Stop bit
-//    while(frameStartTime < (FRAME_STOP_BITS_TIME)) {};
-//    
-//    interrupts();
-
     dmxSendByte(dataArray[frame]);
   }
-
-//  digitalWriteFast(dataPin, LOW);    // Stop bit
-//  delayMicroseconds(20);
-//  digitalWriteFast(dataPin, HIGH);    // Stop bit  
-  // We're done - MTBP is high, same as the stop bit.
   
+  digitalWriteFast(dataPin, LOW);    // Break - 88us
+  delayMicroseconds(88);
+
+  digitalWriteFast(dataPin, HIGH);   // MAB - 8 us
   interrupts();
 }
 
+//=============================================================================================================
+//    Test color
+//=============================================================================================================
+void test_color(void){
+    resetPower();
+    for(int color = 0; color<4; color++){ 
+        Serial.print("Color =  ");
+        Serial.println(color, DEC);
+        for(int output = 0; output < OUTPUT_COUNT; output++) {
+          if(color == 0) {
+            writePixel(output + 1, 50, 0, 0);
+          }
+          else if(color == 1) {
+            writePixel(output + 1, 0, 50, 0);
+          }
+          else if(color == 2) {
+            writePixel(output + 1, 0, 0, 50);
+          }
+          else if(color == 3) {
+            writePixel(output + 1, 50, 50, 50);
+          }
+        }
+        draw();
+        delay(3000);
+    }
+}  
+
+//=============================================================================================================
+//    Setup
+//=============================================================================================================
 
 void setup() {
   // Set the data pin high
@@ -296,35 +431,49 @@ void setup() {
   // Finally, start listening on the serial port
   Serial.begin(115200);
   
+  
   digitalWrite(failLedPin, HIGH);
   digitalWrite(passLedPin, HIGH);
   delay(1000);
   digitalWrite(failLedPin, LOW);
-      Serial.println("Identify pixel: ");
-    
-  
+
 }
 
+//=============================================================================================================
+//    handle Command
+//=============================================================================================================
 
 void handleCommand(char* commandBuffer) {
   char command = commandBuffer[0];
   char p = commandBuffer[1];
+  char q = commandBuffer[2];
+  int parameter = 0;
 
   if(command == COMMAND_OUTPUT_IDENTIFY) {
-    int parameter = p - '0';
-    
+      if(q >= '0' && q<= '9')
+      {
+        parameter = q - '0';
+        if(p >= '0' && p<= '9')
+           parameter += 10*(p - '0');
+      }
+      else if(p >= '0' && p<= '9')
+      {
+           parameter = p - '0';
+      }
+      else
+        Serial.println("Unknow value");
+   
     Serial.print("Identify pixel: ");
-    Serial.println(parameter);
+    Serial.println(parameter, DEC);
      
     for(int output = 0; output < OUTPUT_COUNT; output++) {
       if(output + 1 == parameter) {
-        writePixel(output + 1, 255, 255, 255);
+        writePixel(output + 1, 255, 0, 0);
       }
       else {
         writePixel(output + 1, 1, 1, 1);
       }
     }
-    
     draw();
   }
   
@@ -337,69 +486,55 @@ void handleCommand(char* commandBuffer) {
     for(int output = 0; output < OUTPUT_COUNT; output++) {
 
       if(parameter == 0) {
-        writePixel(output + 1, 255, 0, 0);
+        writePixel(output + 1, 50, 0, 0);
       }
       else if(parameter == 1) {
-        writePixel(output + 1, 0, 255, 0);
+        writePixel(output + 1, 0, 50, 0);
       }
       else if(parameter == 2) {
-        writePixel(output + 1, 0, 0, 255);
+        writePixel(output + 1, 0, 0, 50);
       }
       else {
-        writePixel(output + 1, 255, 255, 255);
+        writePixel(output + 1, 50, 50, 50);
       }
-    }
-    
+    }  
     draw();
-    delay(10);
-    draw();  // TODO: Fix this bug :-P
+
   }
   if(command == COMMAND_PROGRAM_ADDRESSES) {
-    digitalWrite(failLedPin, HIGH);
-    digitalWrite(passLedPin, LOW);
-    Serial.print("Programming addresses");
-    Serial.println("");
-    digitalWrite(powerPin, HIGH);
-    delay(100);
-    digitalWrite(powerPin, LOW);
-    
     programAddresses();
-    programAddresses(); // TODO: Fix this bug :-P
-    
-    //delay(5000);
-
-    digitalWrite(powerPin, HIGH);
-    delay(500);
-    digitalWrite(powerPin, LOW);
-    Serial.print("Programming END");
-    Serial.println("");
-    digitalWrite(failLedPin, LOW);
-    digitalWrite(passLedPin, HIGH);
+  }
+  if(command == COMMAND_ERASE_ADDRESSES) {
+    eraseAddresses();
+  }
+  if(command == COMMAND_TEST_COLOR){
+    test_color();
   }
 }
 
-
-#define COMMAND_LENGTH  2
-char inputBuffer[COMMAND_LENGTH];
-int bufferPosition = 0;
+//=============================================================================================================
+//    Main loop
+//=============================================================================================================
 
 void loop() {
   if(Serial.available() > 0) {
       char c = Serial.read();
       
-      if(c == '\n') {
-        if(bufferPosition == COMMAND_LENGTH) {
+      if(c == 0x0A) {
+        if(bufferPosition <= COMMAND_LENGTH) {
            handleCommand(inputBuffer);
+           inputBuffer[0] = 0;
+           inputBuffer[1] = 0;
+           inputBuffer[2] = 0;
         }
         else {
           Serial.print("Malformed command, length:");
           Serial.println(bufferPosition);
         }
- 
         bufferPosition = 0;       
       }
       else {
-        if(bufferPosition < COMMAND_LENGTH) {
+        if(bufferPosition < COMMAND_LENGTH && c != 0x0D) {
           inputBuffer[bufferPosition] = c;
           bufferPosition++;
         }
@@ -412,9 +547,30 @@ void loop() {
   if(digitalRead(SWITCH_PIN) == LOW) {
     delay(50);
     handleCommand("p");
+    delay(50);
+    inputBuffer[0] = 'i';
+    for(int tile=1; tile<=14; tile++){
+      if(tile <= 9)
+        inputBuffer[1] = tile + '0';
+      else{
+        inputBuffer[1] = '1';
+        inputBuffer[2] = '0'+ tile%10;
+      }
+      handleCommand(inputBuffer);
+      delay(250);
+    }
+    inputBuffer[0] = 0;
+    inputBuffer[1] = 0;
+    inputBuffer[2] = 0;
+    bufferPosition = 0;
+    delay(50);
+    handleCommand("t");
     while(digitalRead(SWITCH_PIN) == LOW) {}
   }
-
+  
+  //===================================================================
+  // Continue test Panel color
+  //===================================================================
     static int counts = 0;
     static int countsPerChange = 300000;
     static int color = 0;
@@ -425,21 +581,22 @@ void loop() {
       
       for(int output = 0; output < OUTPUT_COUNT; output++) {
         if(color == 0) {
-          writePixel(output + 1, 255, 0, 0);
+          writePixel(output + 1, 10, 0, 0);
         }
         else if(color == 1) {
-          writePixel(output + 1, 0, 255, 0);
+          writePixel(output + 1, 0, 10, 0);
         }
         else if(color == 2) {
-          writePixel(output + 1, 0, 0, 255);
+          writePixel(output + 1, 0, 0, 10);
         }
         else if(color == 3) {
-          writePixel(output + 1, 255, 255, 255);
+          writePixel(output + 1, 10, 10, 10);
         }
       }
       
       draw();
       color = (color + 1) % 4;
     }
+
 }
 
